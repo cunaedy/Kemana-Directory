639582cacac4f92a43e977615272685e48d527ad
cunaedy <34178204+cunaedy@users.noreply.github.com>
2020-02-23 23:09:58 +0700

    Update window.php

--- module/qbanner/window.php
+++ module/qbanner/window.php
@@ -5,7 +5,7 @@ $type = strtoupper(mod_param('banner_type'));
 $row = sql_qquery("SELECT * FROM ".$db_prefix."page WHERE group_id='QBANR' ORDER BY RAND() LIMIT 1");
 if (!empty($row)) {
     $output  = "<div style=\"text-align:center\">";
-    $output .= "<a href=\"$row[page_keyword]\"><img src=\"$config[site_url]/public/image/$row[page_image]\" alt=\"$row[page_title]\" /></a>";
+    $output .= "<a href=\"$row[page_keyword]\" target=\"_blank\"><img src=\"$config[site_url]/public/image/$row[page_image]\" alt=\"$row[page_title]\" /></a>";
     $output .= "</div>";
 } else {
     $output = '<!-- no banner found -->';
